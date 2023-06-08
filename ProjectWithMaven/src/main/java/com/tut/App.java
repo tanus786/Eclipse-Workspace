19d6a1597a8d4d69f7c23c72f5bb6a99a8460428
Tanu soni <Tanusoni2312@gmail.com>
2023-06-08 21:10:33 +0530

    Hibernate

--- ProjectWithMaven/src/main/java/com/tut/App.java
+++ ProjectWithMaven/src/main/java/com/tut/App.java
@@ -1,9 +1,10 @@
 package com.tut;
-
+import org.hibernate.SessionFactory;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Project Start" );
+        SessionFactory f
     }
 }
